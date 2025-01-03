2dedbe3c73ec0ece4e1af568c7e6706fbf23f354
Ilovebarbara <140876455+Ilovebarbara@users.noreply.github.com>
2025-01-04 04:45:14 +0800

    Delete clinic_management/myproject directory

--- clinic_management/myproject/medical_records/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import MedicalRecord
-
-admin.site.register(MedicalRecord)
--- clinic_management/myproject/queues/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Queue
-
-admin.site.register(Queue)
-# Register your models here.
